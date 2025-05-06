080671d97bf93fdc55a6f1eec83f03a99c571a0e
Ze2-lucy <zethulucysitole@gmail.com>
2025-05-06 20:18:28 +0200

    Submitting week-7 Assignment

---
+++ answers.sql
@@ -0,0 +1,51 @@
+-- Question 1
+
+CREATE TABLE ProductDetail (
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Products TEXT
+);
+
+
+INSERT INTO ProductDetail (OrderID, CustomerName, Products) VALUES
+(101, 'John Doe', 'Laptop, Mouse'),
+(102, 'Jane Smith', 'Tablet, Keyboard, Mouse'),
+(103, 'Emily Clark', 'Phone');
+
+
+SELECT 
+    OrderID,
+    CustomerName,
+    TRIM(value) AS Product
+
+FROM ProductDetail,
+     UNNEST(string_to_array(Products, ',')) AS value;
+
+
+-- Question 2
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(100)
+);
+
+INSERT INTO Orders (OrderID, CustomerName) VALUES
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
+
+CREATE TABLE OrderDetails (
+    OrderID INT,
+    Product VARCHAR(100),
+    Quantity INT,
+    PRIMARY KEY (OrderID, Product),
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
+
+INSERT INTO OrderDetails (OrderID, Product, Quantity) VALUES
+(101, 'Laptop', 2),
+(101, 'Mouse', 1),
+(102, 'Tablet', 3),
+(102, 'Keyboard', 1),
+(102, 'Mouse', 2),
+(103, 'Phone', 1);
+
